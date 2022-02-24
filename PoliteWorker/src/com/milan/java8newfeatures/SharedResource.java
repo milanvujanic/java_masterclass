6bb014c0e9122feaa82225b91dbb851ec684ced7
milanvujanic <milanvujanic79@gmail.com>
2022-02-24 09:20:08 +0100

    commit

---
+++ PoliteWorker/src/com/milan/java8newfeatures/SharedResource.java
@@ -0,0 +1,18 @@
+package com.milan.java8newfeatures;
+
+public class SharedResource {
+
+    private Worker owner;
+
+    public SharedResource(Worker owner) {
+        this.owner = owner;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+}
